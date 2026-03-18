1564d6dc9d25d4a259eb57ebe94fc8ff04b624d0
MarcoP-at-VICI <marco.piangerelli@vici.it>
2026-03-18 10:43:47 +0100

    Update app.py
    shape_type  --> "line" to "polygon"

--- labelme/app.py
+++ labelme/app.py
@@ -870,7 +870,7 @@ class MainWindow(QtWidgets.QMainWindow):
         for length, line_coords in filtered_lines:
             x1, y1, x2, y2 = line_coords
             # shape = Shape(label="Linea", shape_type="line")
-            shape = Shape(label="Linea", shape_type="linestrip") # cambiamo shape_type da "line" a "linestrip". Un linestrip può avere un numero variabile di punti (da 2 a infiniti).
+            shape = Shape(label="Linea", shape_type="polygon") # cambiamo shape_type da "line" a "polygon". Un polygin può avere un numero variabile di punti (da 2 a infiniti).
             shape.addPoint(QtCore.QPointF(x1, y1))
             shape.addPoint(QtCore.QPointF(x2, y2))
            
